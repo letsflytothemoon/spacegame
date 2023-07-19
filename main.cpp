c70da02f774a1fdca5ff3a5c6dbef23e7330d124
Dmitriy L <losev3628@gmail.com>
2023-07-19 22:43:25 +1000

    .!.

--- main.cpp
+++ main.cpp
@@ -2,16 +2,20 @@
 #include "vector.h"
 #include "guid.h"
 #include "game.h"
+#include "somelib/some.h"
+
 
 int main()
 {
     
     
-    PhysicalObject physicalObject;
-    SpaceGameObject& ref = physicalObject;
+    //PhysicalObject physicalObject;
+    //SpaceGameObject& ref = physicalObject;
     
-    std::cout << physicalObject << std::endl;
-    std::cout << ref << std::endl;
+    //std::cout << physicalObject << std::endl;
+    //std::cout << ref << std::endl;
+
+    std::cout << GetVeryImportantMessage() << std::endl;
 
     
 
